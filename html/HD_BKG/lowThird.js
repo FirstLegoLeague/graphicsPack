c83455d3aaa121437d7dfe689a0055cfdf87292e
rikkertkoppes <rikkert@rikkertkoppes.com>
2015-02-06 11:22:08 +0100

    Differentiate between show and persist

--- html/HD_BKG/lowThird.js
+++ html/HD_BKG/lowThird.js
@@ -1,45 +1,46 @@
 var lowThird = (function() {
-	var visible = false;
+    var visible = false;
     var timer;
-    
+
     function getLowerThird() {
         return document.getElementById('lowThird');
     }
-    function showLowerThird() {
-        getLowerThird().className = 'visible';
+    function persist() {
+        getLowerThird().className = '';
         if (timer) {
             clearTimeout(timer);
             timer = null;
         }
     }
-    function showThenHideLowerThird() {
-        showLowerThird();
+    function showLowerThird() {
+        persist();
         timer = setTimeout(hideLowerThird,5000);
     }
     function hideLowerThird() {
-        getLowerThird().className = '';
+        getLowerThird().className = 'hidden';
     }
     function setText(txt1,txt2) {
-        getLowerThird().innerHTML = '<b>'+(txt1||'')+'</b><br>'+(txt2||'');
+        if (txt1 || txt2) {
+            getLowerThird().innerHTML = '<b>'+(txt1||'')+'</b><br>'+(txt2||'');
+        }
     }
     function doCommand(cmd) {
         switch(cmd) {
             case 'show': showLowerThird(); break;
-            case 'showThenHide': showThenHideLowerThird(); break;
+            case 'persist': persist(); break;
             case 'hide': hideLowerThird(); break;
         }
     }
     function toggle() {
-    	visible ? hideLowerThird() : showLowerThird();
+        visible ? hideLowerThird() : showLowerThird();
     }
-    
 
     return {
-    	command: doCommand,
-    	set: setText,
-    	show: showLowerThird,
-    	hide: hideLowerThird,
-    	showThenHide: showThenHideLowerThird,
-    	toggle: toggle
+        command: doCommand,
+        set: setText,
+        show: showLowerThird,
+        hide: hideLowerThird,
+        persist: persist,
+        toggle: toggle
     };
 }());
